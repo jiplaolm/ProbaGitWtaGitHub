36de8758cf0e5c3c31f06ad9db99fa52a82b4c20
Mikel Larrañaga <mikel.larranaga@ehu.eus>
2022-05-28 08:29:50 +0200

    Azkeneko aldaketa

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,10 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Beste mezu bat");
+        metodoBerriBat();
+    }
+
+    private static void metodoBerriBat() {
+        System.out.println("Beste metodo bat");
     }
 }
